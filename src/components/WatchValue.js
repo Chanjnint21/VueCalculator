9390d325c9b9d692d50c7b26193d23e58a36caa6
Chanjnint21 <limchanjnint@gmail.com>
2023-05-22 17:52:34 +0700

    Watch the input Value

--- src/components/WatchValue.js
+++ src/components/WatchValue.js
@@ -1,6 +1,17 @@
-export default function Ans(newVal, oldVal){
+//functions to check if the value inp by user in string or any scpecial characters that're nor number
+
+export function Value1(newVal){
+    //check if the inp is NaN
+    if(isNaN(newVal)){ 
+        return this.showAlert = true; // make the alert appear
+    } else {
+        return this.showAlert = false; //make the alert disappear
+    }
+}
+export function Value2(newVal){
     if(isNaN(newVal)){
-        console.log(oldVal)
         return this.showAlert = true;
+    } else {
+        return this.showAlert = false;
     }
 }

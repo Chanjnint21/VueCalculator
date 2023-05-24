1616979fa310bad5e9b43dd605d9c7dab7d22750
Chanjnint21 <limchanjnint@gmail.com>
2023-05-24 17:55:17 +0700

    remove btn disabled source code

--- src/components/WatchValue.js
+++ src/components/WatchValue.js
@@ -4,24 +4,15 @@ export function Value1(newVal){
     //check if the inp is NaN
     if(isNaN(newVal)){ 
         return this.showAlert = true; // make the alert appear
-    } 
-    // else if(newVal !== ''){
-    //     return this.isFormValid= true
-    // }  
-    else {
-        // this.isFormValid= false
+    } else {
         return this.showAlert = false; //make the alert disappear
     }
 }
+
 export function Value2(newVal){
     if(isNaN(newVal)){ 
         return this.showAlert = true;
-    } 
-    // else if(newVal !== ''){
-    //     return this.isFormValid= true
-    // }  
-    else {
-        // this.isFormValid= false
+    } else {
         return this.showAlert = false; 
     }
 }

8bbc4c20aad13f9950b2640fd2064b9ace2498b1
Chanjnint21 <limchanjnint@gmail.com>
2023-05-24 17:54:48 +0700

    fix props

--- src/components/VueCalculator.vue
+++ src/components/VueCalculator.vue
@@ -1,39 +1,47 @@
 <template>
-    <v-container class=" area-calculator bg-surface-variant">
-        <v-row>
-            <v-col cols="12"><v-alert text danse outlined type="warning" v-if="showAlert" :value="showAlert" @input="showAlert = false" >Value that are not number will case the error in calculating !</v-alert></v-col>
-            <v-col class="mt-5" cols="5"><v-text-field type="text" label="1st Value" v-model="Value1" outlined></v-text-field></v-col>
-            <v-col class="d-flex align-center justify-center"><p v-html="method"></p></v-col>
-            <v-col class="mt-5" cols="5"><v-text-field type="text" label="2nd Value" v-model="Value2" outlined></v-text-field></v-col>
-            <v-col cols="12"><v-text-field label="=" variant="solo" :value="Ans" model-value="0" readonly ></v-text-field></v-col>
-        </v-row>
-        <v-row class="text-center">
-            <!-- <v-form v-model="isFormValid"></v-form> -->
-            <v-col cols="2"><v-btn @click="ADD" :disabled="!isFormValid"> + </v-btn></v-col>
-            <v-col cols="2"><v-btn @click="SUB"> - </v-btn></v-col>
-            <v-col cols="2"><v-btn @click="MULTI"> &times; </v-btn></v-col>
-            <v-col cols="2"><v-btn @click="DEV"> &divide; </v-btn></v-col>
-            <v-col cols="4"><v-btn :color="Rcolor" v-on:click="reset" block>C</v-btn></v-col>
-        </v-row>
-    </v-container>
+    <v-main class="d-flex align-center justify-center" :id=" MainID ">
+        <v-container class="area-calculator bg-surface-variant rounded-lg px-12 " :id=" ContainerID ">
+            <v-row >
+                <v-col cols="12"><v-alert text danse outlined type="warning" v-if="showAlert" :value="showAlert"
+                        @input="showAlert = false">Value that are not number will case the error in calculating
+                        !</v-alert></v-col>
+                <v-col class="mt-5" cols="5"><v-text-field type="text" color="green" label="1st Value" v-model="Value1"
+                        outlined></v-text-field></v-col>
+                <v-col class="d-flex align-center justify-center">
+                    <p v-html="method"></p>
+                </v-col>
+                <v-col class="mt-5" cols="5"><v-text-field type="text" color="green" label="2nd Value" v-model="Value2"
+                        outlined></v-text-field></v-col>
+                <v-col cols="12"><v-text-field label="=" color="green" variant="solo" :value="Ans"
+                        readonly></v-text-field></v-col>
+            </v-row>
+            <v-row class="text-center">
+                <v-col cols="2"><v-btn @click="ADD" :disabled="!isBtnValid"> + </v-btn></v-col>
+                <v-col cols="2"><v-btn @click="SUB" :disabled="!isBtnValid"> - </v-btn></v-col>
+                <v-col cols="2"><v-btn @click="MULTI" :disabled="!isBtnValid"> &times; </v-btn></v-col>
+                <v-col cols="2"><v-btn @click="DEV" :disabled="!isBtnValid"> &divide; </v-btn></v-col>
+                <v-col cols="4"><v-btn :color="Rcolor" v-on:click="reset" block> C </v-btn></v-col>
+            </v-row>
+        </v-container>
+    </v-main>
 </template>
 
 <script>
-import {Value1, Value2} from './WatchValue.js'
+import '../assets/styles.css'
+import { Value1, Value2 } from './WatchValue.js'
 
-export default{
+export default {
     props: {
-        Rcolor:{
+        Rcolor: {
             type: String,
             default: 'warning',
-        }, 
-        // method:{
-        //     type: String,
-        //     default: "...."
-        // }
+        },
+        Switch: {
+            type: Boolean
+        }
     },
-    data(){
-        return{
+    data() {
+        return {
             Value1: '',
             Value2: '',
             showAlert: '',
@@ -42,56 +50,73 @@ export default{
             sub: false,
             multi: false,
             dev: false,
-            isFormValid: ''
+            MainID: '',
+            ContainerID: ''
         }
     },
-    watch:{
+    watch: {
         Value1: Value1,
         Value2: Value2,
+        Switch() {
+            if (this.Switch) {
+                this.MainID = 'MainID';
+                this.ContainerID = 'ContainerID'
+            }
+            else {
+                this.MainID = ''
+                this.ContainerID = ''
+            }
+        }
     },
-    computed:{
-        Ans(){
+    computed: {
+        Ans() {
             //convert the inp value to number 
             let V1 = Number(this.Value1)
             let V2 = Number(this.Value2)
 
             //check operation condition
-            if(this.add){
+            if (this.add) {
                 return V1 + V2;
-            } else if( this.sub){
+            } else if (this.sub) {
                 return V1 - V2;
-            }else if( this.multi){
+            } else if (this.multi) {
                 return V1 * V2;
-            }else if( this.dev){
+            } else if (this.dev) {
                 return V1 / V2;
             }
             return "";
         },
+        isBtnValid() {
+            if (this.Value1 === '' || this.Value2 === '') {
+                return 0
+            }
+            return 1
+        }
     },
-    methods:{
-        reset: function(){
+    methods: {
+        reset: function () {
             this.method = '....'
-           this.Value1='';
-           this.Value2='';
-           this.add = this.sub = this.multi = this.dev = false;
+            this.Value1 = '';
+            this.Value2 = '';
+            this.add = this.sub = this.multi = this.dev = false;
         },
-        ADD: function(){
-            this.method="+"
+        ADD: function () {
+            this.method = "+"
             this.add = true;
             this.sub = this.multi = this.dev = false;
         },
-        SUB: function(){
-            this.method="-"
+        SUB: function () {
+            this.method = "-"
             this.sub = true;
             this.add = this.multi = this.dev = false;
         },
-        MULTI: function(){
-            this.method= "&times;"
+        MULTI: function () {
+            this.method = "&times;"
             this.multi = true;
             this.sub = this.add = this.dev = false;
         },
-        DEV: function(){
-            this.method= "&divide;"
+        DEV: function () {
+            this.method = "&divide;"
             this.dev = true;
             this.sub = this.multi = this.add = false;
         },
@@ -117,7 +142,8 @@ export default{
 </script>
 
 <style>
-.area-calculator{
-    width: 600px;
-}
+/* .area-calculator {
+    width: 700px;
+    height: 400px;
+} */
 </style>

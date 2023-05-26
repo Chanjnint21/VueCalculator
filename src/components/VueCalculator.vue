10c79397e8ef50277506df18cd0d6a4797159d9f
Chanjnint21 <limchanjnint@gmail.com>
2023-05-26 17:32:46 +0700

    Added Float parse to two decimal

--- src/components/VueCalculator.vue
+++ src/components/VueCalculator.vue
@@ -1,27 +1,43 @@
 <template>
-    <v-main class="d-flex align-center justify-center" :id=" MainID ">
-        <v-container class="area-calculator bg-surface-variant rounded-lg px-12 " :id=" ContainerID ">
-            <v-row >
+    <v-main class="d-flex align-center justify-center" :id="MainID">
+        <v-container class="area-calculator bg-surface-variant rounded-lg px-12 " :id="ContainerID">
+
+            <v-row>
+                <!-- Alert -->
                 <v-col cols="12"><v-alert text danse outlined type="warning" v-if="showAlert" :value="showAlert"
                         @input="showAlert = false">Value that are not number will case the error in calculating
                         !</v-alert></v-col>
+
+                <!-- value input 1-->
                 <v-col class="mt-5" cols="5"><v-text-field type="text" color="green" label="1st Value" v-model="Value1"
                         outlined></v-text-field></v-col>
+
+                <!-- display the method -->
                 <v-col class="d-flex align-center justify-center">
                     <p v-html="method"></p>
                 </v-col>
+
+                <!-- value input 2-->
                 <v-col class="mt-5" cols="5"><v-text-field type="text" color="green" label="2nd Value" v-model="Value2"
                         outlined></v-text-field></v-col>
-                <v-col class="d-flex align-center justify-center mb-2" ><CalHistory :history="history"></CalHistory></v-col>        
+
+                <!-- icon click for histor -->
+                <v-col class="d-flex align-center justify-center mb-2">
+                    <CalHistory :history="history"></CalHistory>
+                </v-col>
+
+                <!-- Result (readonly text field) -->
                 <v-col cols="12"><v-text-field label="=" color="green" variant="solo" :value="Ans"
                         readonly></v-text-field></v-col>
             </v-row>
+            <!-- method btns and clear btn -->
             <v-row class="text-center">
                 <v-col cols="2"><v-btn @click="ADD" :disabled="!isBtnValid"> + </v-btn></v-col>
                 <v-col cols="2"><v-btn @click="SUB" :disabled="!isBtnValid"> - </v-btn></v-col>
                 <v-col cols="2"><v-btn @click="MULTI" :disabled="!isBtnValid"> &times; </v-btn></v-col>
                 <v-col cols="2"><v-btn @click="DEV" :disabled="!isBtnValid"> &divide; </v-btn></v-col>
-                <v-col cols="4"><v-btn :color="Rcolor" v-on:click="reset" block> C </v-btn></v-col>
+                <v-col cols="2"><v-btn :color="Rcolor" v-on:click="reset" block> C </v-btn></v-col>
+                <v-col class="mb-5" cols="2"><v-btn v-on:click="FloatParse" color="success" >.00</v-btn></v-col>
             </v-row>
         </v-container>
     </v-main>
@@ -29,9 +45,6 @@
 
 <script>
 import '../assets/styles.css'
-import { WatchValue1, WatchValue2, Switch } from './property/Watcher.js'
-import { showAlert, isBtnValid } from './property/Computed.js'
-import CalHistory from './CalHistory.vue'
 
 export default {
     props: {
@@ -43,35 +56,39 @@ export default {
             type: Boolean
         }
     },
-    components:{
-        CalHistory
-    },
     data() {
         return {
             Value1: '',
             Value2: '',
-            V1Alert: '',
-            V2Alert: '',
             method: '....',
             Ans: '',
-            //add: false,
-            //sub: false,
-            //multi: false,
-            //dev: false,
             MainID: '',
             ContainerID: '',
-            history: []
+            history: [],
         }
     },
     watch: {
-        Value1: WatchValue1,
-        Value2: WatchValue2,
-        Switch: Switch
+        //check the switch if it true toggle Id to dark mode
+        Switch() {
+            if (this.Switch) {
+                this.MainID = 'MainID';
+                this.ContainerID = 'ContainerID'
+            }
+            else {
+                this.MainID = ''
+                this.ContainerID = ''
+            }
+        },
     },
     computed: {
-        //Ans: Ans,
-        showAlert: showAlert,
-        isBtnValid: isBtnValid,
+        // check if the text field of value 1 or 2 is not number show alert
+        showAlert() {
+            return isNaN(this.Value1) || isNaN(this.Value2)
+        },
+        // check if the text field of value 1 or 2 empty disable button
+        isBtnValid() {
+            return this.Value1 === '' || this.Value2 === ''
+        },
     },
     methods: {
         reset: function () {
@@ -79,57 +96,37 @@ export default {
             this.Value1 = '';
             this.Value2 = '';
             this.Ans = ''
-            //this.add = this.sub = this.multi = this.dev = false;
         },
-        ToHistory(meth){
-            this.history.push(`${this.Value1} ${meth} ${this.Value1} = ${this.Ans}`)
+        ToHistory(Symbol) {
+            this.history.push(`${this.Value1} ${Symbol} ${this.Value2} = ${this.Ans}`)
         },
-        ADD: function(){
+        FloatParse() {
+            if(this.Ans === '') return ; //if the Ans is empty dun parse
+            this.Ans = Math.round(this.Ans * 100) / 100
+            this.ToHistory(this.method) ; // if the Ans already parse, just return
+            
+        },
+        ADD: function () {
             this.method = "+"
-            const sum = Number(this.Value1) + Number(this.Value2)
-            this.Ans = sum;
+            this.Ans = Number(this.Value1) + Number(this.Value2);
             this.ToHistory('+')
         },
-        SUB: function(){
+        SUB: function () {
             this.method = "-"
-            const sum = Number(this.Value1) - Number(this.Value2)
-            this.Ans = sum;
+            this.Ans = Number(this.Value1) - Number(this.Value2);
             this.ToHistory('-')
         },
-        MULTI: function(){
+        MULTI: function () {
             this.method = "&times;"
-            const sum = Number(this.Value1) * Number(this.Value2)
-            this.Ans = sum;
+            this.Ans = Number(this.Value1) * Number(this.Value2);
             this.ToHistory('&times;')
         },
-        DEV: function(){
+        DEV: function () {
             this.method = "&divide;"
-            const sum = Number(this.Value1) / Number(this.Value2)
-            this.Ans = sum;
+            this.Ans = Number(this.Value1) / Number(this.Value2);
             this.ToHistory('&divide;')
         },
 
-        //---------------------when use computed--------------------
-        // ADD: function () {
-        //     this.method = "+"
-        //     this.add = true;
-        //     this.sub = this.multi = this.dev = false;
-        // },
-        // SUB: function () {
-        //     this.method = "-"
-        //     this.sub = true;
-        //     this.add = this.multi = this.dev = false;
-        // },
-        // MULTI: function () {
-        //     this.method = "&times;"
-        //     this.multi = true;
-        //     this.sub = this.add = this.dev = false;
-        // },
-        // DEV: function () {
-        //     this.method = "&divide;"
-        //     this.dev = true;
-        //     this.sub = this.multi = this.add = false;
-        // },
     },
 }
 </script>
